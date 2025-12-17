fdddd43903c178bedcd2c582a09fd9777cfe84e4
Carlos Bucay <cabucay@espe.edu.ec>
2025-12-16 22:20:37 -0500

    Update 2025_12_15_153757_create_clientes_table.php
    Cambio archivos Create clientes table

---
+++ database/migrations/2025_12_15_153757_create_clientes_table.php
@@ -0,0 +1,26 @@
+
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up()
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('razon_social')->nullable();
+            $table->string('email')->nullable();
+            $table->string('telefono')->nullable();
+            $table->string('direccion')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('clientes');
+    }
+};
