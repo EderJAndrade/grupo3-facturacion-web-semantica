cb7f7736bd74587a2b7b289120033566d5660256
Fgcisneros <66039510+Fgcisneros@users.noreply.github.com>
2025-12-17 15:52:29 -0500

    Add Producto model with fillable properties

--- app/Models/Producto.php
+++ app/Models/Producto.php
@@ -1,1 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Producto extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['codigo','nombre','descripcion','precio_unitario','stock'];
+
+    public function detalleFacturas()
+    {
+        return $this->hasMany(DetalleFactura::class);
+    }
+}
+
 
